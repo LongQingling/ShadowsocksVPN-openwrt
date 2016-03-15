8a587d4f878b55609a3be4644e25175cc30e6ef5
bettermanbao <jackybao_sh@msn.com>
2016-03-15 08:40:17 +0800

    Update shadowsocks-libev-custom.lua

--- files/shadowsocks-libev-custom.lua
+++ files/shadowsocks-libev-custom.lua
@@ -14,7 +14,7 @@ function f.handle(self, state, data)
 	if state == FORM_VALID then
 		if data.conf then
 			fs.writefile(conffile, data.conf:gsub("\r\n", "\n"))
-			luci.sys.call("/etc/init.d/dnsmasq restart")
+			luci.sys.call("/etc/init.d/dnsmasq restart && ipset flush gfwlist")
 		end
 	end
 	return true
